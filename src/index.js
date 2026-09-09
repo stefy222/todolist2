1fbce7a70fcbdc29b9a8dba91adc4134813100d6
stefy222 <stefpau220@gmail.com>
2026-09-09 12:35:33 -0400

    TOD-1: Inicializar proyecto base

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConnection } from "./db/connection.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "API inicializada correctamente" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "NOT FOUND",
+    message: "La ruta solicitada no existe",
+  });
+});
+
+const startServer = async () => {
+  await dbConnection();
+
+  app.listen(PORT, () => {
+    console.log('Servidor iniciado en http://localhost:${PORT}');
+  });
+};
+
+startServer();
