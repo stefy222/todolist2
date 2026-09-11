5d40502aa59b87afa85ee26160197373af136523
stefy222 <stefpau220@gmail.com>
2026-09-11 18:03:58 -0400

    TOD-7: Autenticación Bcrypt + JWT

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+
 import { randomUUID } from "crypto";
 import { pool } from "../db/connection.js";
 
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,1 +1,6 @@
 export const isValidId = (id) => typeof id === 'string' && id.trim().length === 36;
+
+export const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
