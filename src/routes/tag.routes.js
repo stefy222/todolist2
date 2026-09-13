23a31e85eda5c47353c3079edbe5be2024c9d304
Rosibel Paucara <stefpau220@gmail.com>
2026-09-12 23:46:59 -0400

    TOD-8: Middleware

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 
 import {index,store,show,update,destroy } from "../controllers/tag.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+router.use(authMiddleware);
 router.post("/tags",index)
 
 router.post("/tags",store);
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,7 +2,10 @@ import express from "express";
 
 import {index,store,show,update,destroy } from "../controllers/task.controller.js";
 
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
+router.use(authMiddleware);
 router.post("/task",index)
 
 router.post("/task",store);
