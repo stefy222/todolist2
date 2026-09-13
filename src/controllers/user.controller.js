76956258a2b4d2023ac56c19859d93abd8036a92
Rosibel Paucara <stefpau220@gmail.com>
2026-09-12 23:40:29 -0400

    Fix: add login in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 import { randomUUID } from "crypto";
 import { pool } from "../db/connection.js";
+import { userDecorator } from "../decorators/user.decorator.js";
 
 export const createUser = async (req, res) => {
   try {
@@ -10,6 +11,12 @@ export const createUser = async (req, res) => {
     if (!name || !email || !password) {
       return res.status(400).json({
         message: "Todos los campos son obligatorios"
+      });
+    }
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        message: "La contraseña debe tener al menos 8 caracteres"
       });
     }
 
@@ -36,15 +43,78 @@ export const createUser = async (req, res) => {
 
     res.status(201).json({
       message: "Usuario creado correctamente",
-      user: {
+      user: userDecorator({
         id,
         name,
         email
-      }
+      })
     });
 
   } catch (error) {
     console.error("Error al crear usuario:", error);
+
+    res.status(500).json({
+      message: "Error interno del servidor"
+    });
+  }
+};
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "El correo y la contraseña son obligatorios"
+      });
+    }
+
+    const [users] = await pool.query(
+      `SELECT id, name, email, password
+       FROM users
+       WHERE email = ?`,
+      [email]
+    );
+
+    if (users.length === 0) {
+      return res.status(401).json({
+        message: "Correo o contraseña incorrectos"
+      });
+    }
+
+    const user = users[0];
+
+    const passwordValid = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "Correo o contraseña incorrectos"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1 hora"
+      }
+    );
+
+    res.status(200).json({
+      message: "Inicio de sesión exitoso",
+      token,
+      user: userDecorator(user)
+    });
+
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
 
     res.status(500).json({
       message: "Error interno del servidor"
