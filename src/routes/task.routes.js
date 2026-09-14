fabe3aa9dbea7990ca40c0e0cee0d9e77dccb108
stefy222 <stefpau220@gmail.com>
2026-09-14 19:28:42 -0400

    TOD-6: Módulo de Tareas  (#8)
    * TOD-1: Inicialización de proyecto base
    
    * TOD-2: Creación base de datos
    
    * TOD-3: Crear usuario
    
    * TOD-4: Modulo Categorias
    
    * Fix: Arreglos sugeridos
    
    * Firs commit: Tags
    
    * Agregar Decorator
    
    * Add controller
    
    * TOD-6: Modulo de tareas

---
+++ src/routes/task.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import {index,store,show,update,destroy } from "../controllers/task.controller.js";
+
+const router = express.Router();
+router.post("/",index)
+
+router.post("/",store);
+
+router.get("/",show);
+
+router.put("/:id", update);
+
+router.delete("/:id", destroy);
+
+export default router;
