36fcd0907c63f9fecad1c39edddedb1cafe094d9
stefy222 <stefpau220@gmail.com>
2026-09-14 22:28:58 -0400

    TOD-8: Middleware de autenticación (#10)
    * TOD-1: Inicialización de proyecto base
    
    * TOD-2: Creación base de datos
    
    * TOD-3: Crear usuario
    
    * TOD-4: Modulo Categorias
    
    * Fix: Arreglos sugeridos
    
    * Firs commit: Tags
    
    * Agregar Decorator
    
    * Add controller
    
    * TOD-6: Modulo de tareas
    
    * Fix: Borrar asociación con etiquetas
    
    * TOD-7: Autenticación Bcrypt + JWT
    
    * install jsonwebtoken
    
    * Fix: add login in controller
    
    * TOD-8: Middleware

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,7 +2,10 @@ import express from "express";
 
 import {index,store,show,update,destroy } from "../controllers/category.controller.js";
 
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router = express.Router();
+router.use(authMiddleware);
+
 router.post("/",index)
 
 router.post("/",store);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createUser, login } from '../controllers/user.controller.js';
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post('/users', createUser);
-router.post('/login', login);
-
+router.post('/login',login);
+router.use(authMiddleware);
 export default router;
